c484eb95b7209a8199dec56c473672181b66c305
agrawalshrawan245 <35890883+agrawalshrawan245@users.noreply.github.com>
2022-07-25 02:46:07 +0900

    Update Heap sort.java

--- Heap sort.java
+++ Heap sort.java
@@ -1,9 +1,4 @@
 public class me {    
-    static void swap(int []a, int []b){
-        int temp = a[0];
-        a[0] = b[0];
-        b[0] = temp;
-    }
 
     public static void main(String[] args){
         int arr[] = {0,3,2,6,44,7,4,6,8,9,16,13,11};
@@ -12,18 +7,22 @@ public class me {
         //Heapfiy
         for(int i = 1; i < len; i++){
             int j = i;
-            while(j > 0 && arr[j] > arr[(j-1)/2]) swap(arr[j], arr[(j - 1) / 2]);
+            while(j > 0 && arr[j] > arr[(j-1)/2]) {
+                arr[j] = arr[j] + arr[(j - 1) / 2] - (arr[(j - 1) / 2] = arr[j]);
+                j = (j - 1) / 2;
+            }
         }
 
         //sort
         for(int i = len - 1; i > 0; i--){
-            swap(arr[i], arr[0]);
+            arr[0]=arr[0]+arr[i]-(arr[i] = arr[0]);
             int j = 0;
             int k = 0;
-            while(j < i && j != k){
+            while(j < i){
                 if(2 * j + 1 < i && arr[k] < arr[2 * j + 1]) k = 2 * j + 1;
                 if(2 * j + 2 < i && arr[k] < arr[2 * j + 2]) k = 2 * j + 2;
-                swap(arr[k], arr[j]);
+                if(j == k) break;
+                arr[k]=arr[k]+arr[j]-(arr[j] = arr[k]);
                 j = k;
             }
         }
